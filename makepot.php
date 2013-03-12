c90bb2676c11308f0006ff2fe3e7661c666357e7
Marko Heijnen <info@markoheijnen.com>
2013-03-12 19:56:00 +0100

    Allow setting the slug via CLI. Fixes #1

--- makepot.php
+++ makepot.php
@@ -544,18 +544,26 @@ class MakePOT {
 // run the CLI only if the file
 // wasn't included
 $included_files = get_included_files();
-if ($included_files[0] == __FILE__) {
+if ( $included_files[0] == __FILE__ ) {
 	$makepot = new MakePOT;
-	if ((3 == count($argv) || 4 == count($argv)) && in_array($method = str_replace('-', '_', $argv[1]), get_class_methods($makepot))) {
-		$res = call_user_func(array(&$makepot, $method), realpath($argv[2]), isset($argv[3])? $argv[3] : null);
-		if (false === $res) {
-			fwrite(STDERR, "Couldn't generate POT file!\n");
-		}
-	} else {
+	$count   = count( $argv );
+
+	if ( $count >= 3 && $count <= 5 && in_array( $method = str_replace( '-', '_', $argv[1] ), get_class_methods( $makepot ) ) ) {
+		$res = call_user_func(
+			array( $makepot, $method ),
+			realpath( $argv[2] ),
+			isset( $argv[3] )? $argv[3] : null,
+			isset( $argv[4] )? $argv[4] : null
+		);
+
+		if ( false === $res )
+			fwrite( STDERR, "Couldn't generate POT file!\n" );
+	}
+	else {
 		$usage  = "Usage: php makepot.php PROJECT DIRECTORY [OUTPUT]\n\n";
 		$usage .= "Generate POT file from the files in DIRECTORY [OUTPUT]\n";
-		$usage .= "Available projects: ".implode(', ', $makepot->projects)."\n";
-		fwrite(STDERR, $usage);
+		$usage .= "Available projects: " . implode( ', ', $makepot->projects )."\n";
+		fwrite( STDERR, $usage );
 		exit(1);
 	}
 }
